5bccc76d1dafc76770d6d3a9af1d0df4202ca562
Giammarco Senatore <giammasena@gmail.com>
2026-01-22 13:50:33 +0100

    add some more docs

--- Board2/Core/Inc/debug.h
+++ Board2/Core/Inc/debug.h
@@ -1,20 +1,63 @@
-/* SEGGER Debugging build configuration */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file debug.h
+ * @brief Debugging configuration macros.
+ *
+ * This file contains preprocessor definitions to control debugging features,
+ * execution modes, and verbosity of various modules.
+ */
+
+/** @brief SEGGER Debugging build configuration */
 #define SEGGER_BUILD 0
 
-// 1: Esegue il codice reale, 0: Simula il carico con HAL_Delay
+/**
+ * @brief Task Execution Mode
+ * 1: Executes real code
+ * 0: Simulates load with HAL_Delay
+ */
 #define REAL_TASK 1
 
-// 1: Abilita stampe di debug, 0: Disabilita stampe di debug
+/**
+ * @brief Global Debug Print Enable
+ * 1: Enables debug prints
+ * 0: Disables debug prints
+ */
 #define PRINT_TASK 0
 
-// 1 per abilitare le stampe, 0 per disabilitarle
+/** 
+ * @brief Main Debug Verbosity
+ * 1 to enable prints, 0 to disable them 
+ */
 #define VERBOSE_DEBUG_MAIN 0
 
-// 1 per abilitare le stampe, 0 per disabilitarle
+/** 
+ * @brief Interrupt Debug Verbosity
+ * 1 to enable prints, 0 to disable them 
+ */
 #define VERBOSE_DEBUG_IT 0
 
-// 1 per abilitare le stampe, 0 per disabilitarle
+/** 
+ * @brief CRC Debug Verbosity
+ * 1 to enable prints, 0 to disable them 
+ */
 #define VERBOSE_DEBUG_CRC 0 
 
-// 1 per abilitare le stampe, 0 per disabilitarle
+/** 
+ * @brief UART Debug Verbosity
+ * 1 to enable prints, 0 to disable them 
+ */
 #define VERBOSE_DEBUG_UART 0
