4bc30f04f7211afc3c285f55474452f27bd7ae2c
Tyusibo <a.tudino@studenti.unisa.it>
2026-01-27 12:07:18 +0100

    Update sync_start.h

--- Board1/Core/Inc/synchronization/sync_start.h
+++ Board1/Core/Inc/synchronization/sync_start.h
@@ -6,18 +6,34 @@
 
 #include "actual_board.h"
 
-/* Configura questo valore: margine di sicurezza per partire "tra poco" */
+/* margine di sicurezza per partire "tra poco" */
 #define SYNC_K_TICKS 10u
 
-void Sync_Init();
+/* Inizializza i pin usati dal sync:
+ * - Board1: in = ACK_IN,  out = SYNC_OUT
+ * - Board2: in = SYNC_IN, out = ACK_OUT
+ */
+void Sync_Init(GPIO_TypeDef *in_port, uint16_t in_pin,
+               GPIO_TypeDef *out_port, uint16_t out_pin);
 
 /* Chiamare all'inizio di OGNI task applicativo */
 void Sync_WaitStart(osEventFlagsId_t flagsSync);
 
 /* Da chiamare nei callback EXTI (HAL_GPIO_EXTI_Callback) */
 void Sync_OnSyncEdgeFromISR(osEventFlagsId_t flagsSync);
-//void Sync_OnAckEdgeFromISR(osEventFlagsId_t flagsSync);
+void Sync_OnAckEdgeFromISR (osEventFlagsId_t flagsSync);
 
-void SyncThread(osEventFlagsId_t flagsSync);
+/* Wrapper: su Board1 setta ACK, su Board2 setta SYNC */
+static inline void Sync_OnEdgeFromISR(osEventFlagsId_t flagsSync)
+{
+#if defined(BOARD1)
+  Sync_OnAckEdgeFromISR(flagsSync);
+#else
+  Sync_OnSyncEdgeFromISR(flagsSync);
+#endif
+}
+
+/* Thread di sincronizzazione (da creare con osThreadNew) */
+void SyncThread(void *argument);
 
 #endif /* INC_SYNCHRONIZATION_SYNC_START_H_ */
